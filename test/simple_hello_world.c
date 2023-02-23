82a6fd45761e83c3b689d5ab5f7c0b094cc1ebaa
Prashanth Duvvuri <prashanthsedan@gmail.com>
2023-02-23 17:37:53 -0500

    changes to the test file

--- test/simple_hello_world.c
+++ test/simple_hello_world.c
@@ -8,6 +8,11 @@ int main(int argc, char *argv[]) {
   MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
 
   printf("My rank is %d \n", myrank);
+  int i;
+  for(i=0;i<1000;i++) {
+    printf("printing %d \n", i);
+    sleep(1);
+  }
 
   MPI_Finalize();
   return 0;
